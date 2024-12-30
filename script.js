cb446fe509eace89bb15b2cf3338aaf037fd52d2
Virender Kumar <virenderkumar9205@gmail.com>
2024-12-30 14:19:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let todoInput = document.querySelector(".input");
+let addTodoButton = document.querySelector(".button");
+let showTodos = document.querySelector(".todos-container");
+let todo = "";
+
+let localData = JSON.parse(localStorage.getItem("todos"));
+let todoList = localData || [];
+
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (param) {
+        var number = Math.random() * 16 | 0,
+            randomNumber = param == 'x' ? number : (number & 0x3 | 0x8);
+        return randomNumber.toString(16);
+    });
+}
+
+addTodoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    todo = todoInput.value;
+    if (todo.length > 0) {
+        todoList.push({
+            todo,
+            id: uuid(),
+            isCompleted: false
+        });
+        renderTodoList(todoList);
+        localStorage.setItem("todos", JSON.stringify(todoList));
+        todoInput.value = "";
+    }
+});
+
+showTodos.addEventListener("click", (e) => {
+    e.preventDefault();
+    let key = e.target.dataset.key;
+    let delTodoKey = e.target.dataset.todokey;
+    todoList = todoList.map((todo) =>
+        todo.id === key ? {
+            ...todo,
+            isCompleted: !todo.isCompleted
+        } : todo
+    );
+    todoList = todoList.filter((todo) => todo.id !== delTodoKey);
+    localStorage.setItem("todos", JSON.stringify(todoList));
+    renderTodoList(todoList);
+});
+
+function renderTodoList(todoList) {
+    showTodos.innerHTML = todoList.map(
+        ({
+            todo,
+            id,
+            isCompleted
+        }) =>
+        `<div class="todo relative"> <input id="item-${id}" data-key=${id} class="t-checkbox t-pointer" type="checkbox" ${
+        isCompleted ? "checked" : ""
+      }> <label data-key=${id} class="todo-text t-pointer ${
+        isCompleted ? "checked-todo" : ""
+      }" for="item-${id}"> ${todo} </label> <button class="absolute right-0 button cursor">
+      <span data-todokey=${id}  class="del-btn material-icons-outlined">delete</span>
+            </button> </div>`
+    );
+}
+
+renderTodoList(todoList);
